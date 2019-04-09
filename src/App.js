291140b62c5aa945e32a38e0a2590664be167d07
Sungjin <myoon567@gamil.com>
2019-04-09 15:46:37 +0000

    update movie list

--- src/App.js
+++ src/App.js
@@ -107,33 +107,19 @@ class App extends Component {
         </header>
         <ul>
           <h2>Favorite Movies</h2>
+		  {
+          	profiles.map( profile => {
+            	const userName = users[profile.userID].name;
+				const favMovie = movies[profile.favoriteMovieID].name;
+				return(
+                	<li key={profile.id}>
+						{`${userName}'s favorite movie is ${favMovie} `}
+					</li>
+                );
+            
+				} )
+          }
 
-          {/*
-          For each object in the profiles array, make a list of JSX elements.
-          The map function returns a new array, which will
-          only have list elements in it. React will automatically know how to
-          render an array of JSX objects.
-          */}
-          {profiles.map(profile => {
-            const userName = users[profile.userID].name;
-            const favMovieName = movies[profile.favoriteMovieID].name;
-
-            return (
-              /*
-              You need to include the `key` property every time you create list
-              elements in React. The key property is necessary because it enables
-              React to perform reconciliation
-              (https://reactjs.org/docs/reconciliation.html)
-              */
-              <li key={profile.id}>
-                {/* If the syntax below looks unfamiliar, please take the following
-              course:
-              https://www.udacity.com/course/es6-javascript-improved--ud356
-                */}
-                <p>{`${userName}\'s favorite movie is "${favMovieName}."`}</p>
-              </li>
-            );
-          })}
         </ul>		
       </div>
     );
