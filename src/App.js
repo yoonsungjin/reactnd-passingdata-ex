4dd76506d312c23447efc8618d47ba64bb30ec21
Sungjin <myoon567@gamil.com>
2019-04-09 15:33:16 +0000

    add movie list

--- src/App.js
+++ src/App.js
@@ -105,7 +105,36 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>Favorite Movies</h2>
+        <ul>
+          <h2>Favorite Movies</h2>
+
+          {/*
+          For each object in the profiles array, make a list of JSX elements.
+          The map function returns a new array, which will
+          only have list elements in it. React will automatically know how to
+          render an array of JSX objects.
+          */}
+          {profiles.map(profile => {
+            const userName = users[profile.userID].name;
+            const favMovieName = movies[profile.favoriteMovieID].name;
+
+            return (
+              /*
+              You need to include the `key` property every time you create list
+              elements in React. The key property is necessary because it enables
+              React to perform reconciliation
+              (https://reactjs.org/docs/reconciliation.html)
+              */
+              <li key={profile.id}>
+                {/* If the syntax below looks unfamiliar, please take the following
+              course:
+              https://www.udacity.com/course/es6-javascript-improved--ud356
+                */}
+                <p>{`${userName}\'s favorite movie is "${favMovieName}."`}</p>
+              </li>
+            );
+          })}
+        </ul>		
       </div>
     );
   }
